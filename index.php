edeaa3c2b6b032cb92c1ab7a59484cd863179e92
Mari1548 <124880795+Mari1548@users.noreply.github.com>
2024-02-23 09:37:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AULA 23-02</title>
+</head>
+<body>
+    <h1>Meu primeiro PHP</h1>
+
+    <?php
+    date_default_timezone_set("America/Sao_Paulo");
+    echo"oioi mundo". "&#10083;&#65039;<br>";
+    echo date("d/m/Y");
+    echo"<br>";
+    echo date("g/i/s");
+    ?>
+</body>
+</html>
