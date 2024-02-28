aafaf21c5e6602a91652edb2211a21e1fd44780f
Mari1548 <124880795+Mari1548@users.noreply.github.com>
2024-02-28 12:04:57 -0300

    Add files via upload

---
+++ mariana/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE >
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Variavel</title>
+  <link rel="stylesheet" type="text/css" href="css.css" media="screen" />
+</head>
+<body>
+  <?php
+    $nome = "Mariana";
+
+    const SOBRENOME = "Souza";
+    echo "meu nome é $nome " . SOBRENOME;
+  ?>
+  
+</body>
+</html>
